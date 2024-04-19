8cf6e25360fc3f036900d3f8b55563bbdee33df1
Gaurang0506 <156852398+Gaurang0506@users.noreply.github.com>
2024-04-19 12:53:58 -0400

    Add files via upload

--- project.py
+++ project.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 
 
 def fetch_hotel_data():
-    con = sqlite3.connect('data.db')
+    con = sqlite3.connect('hotel_1.db')
     cursor = con.cursor()
     cursor.execute('SELECT * FROM hotel_data')
     columns = [col[0] for col in cursor.description]  # Get column names
     rows = cursor.fetchall()
     data = [dict(zip(columns, row)) for row in rows]  # Convert rows to dictionaries
     con.close()
+    print(data)
     return data
-
 @app.route('/')
 def home():
     return render_template("home.html")
@@ -21,9 +21,7 @@ def home():
 # Route to display the hotel data
 @app.route('/view_data.html')
 def view_data():
-    return render_template("view_data.html", hotel_data=fetch_hotel_data)
-
-
+    return render_template("view_data.html", hotel_data=fetch_hotel_data())
 
 @app.route('/about.html')
 def about():
